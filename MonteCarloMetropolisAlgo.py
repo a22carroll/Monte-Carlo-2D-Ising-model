8732610b9e800f9c37ad42511ef0c023f6058472
a22carroll <a22carroll@gmail.com>
2025-08-24 16:41:50 -0400

    refined code to display multiple different temperatures as well as the initail state

--- MonteCarloMetropolisAlgo.py
+++ MonteCarloMetropolisAlgo.py
@@ -1,59 +1,43 @@
-import math 
+# Monte Carlo simulation of the 2D Ising model at different temperatures
+# Using Metropolis algorithm
 import numpy as np
-import sympy as sp
-from scipy import integrate as integ
 from matplotlib import pyplot as plt
-import random
 
-
-#Monte Carlo simulation of  a 2D Ising model using Metropolis algorithm
-
-
-
-
-
-
-#8.1
-#IC
-T = 2
-size = 10
+# Parameters
+size = 20  # Size of the grid
 iteration = 100*(size**2)
-M = np.zeros(shape=(size, size))
+temperatures = [4.0, 2.0, 1.0]  # Three different temperatures
 
-#M Values
-for a in range(0,size):
-  for b in range(0,size):
-    M[a][b] = np.random.choice([-1, 1])
-#Delta U func
-def U(x,y):
+# Initialize ONE grid that we'll use for all simulations
+M_initial = np.random.choice([-1, 1], size=(size, size))
 
-  if x == 0:
-    left = size-1
-  else:
-    left = x - 1
-  if x == size-1:
-    right = 0
-  else:
-    right = x + 1
-  if y == 0:
-    top = size-1
-  else:
-    top = y - 1
-  if y == size-1:
-    bottom = 0
-  else:
-    bottom = y + 1
+# Energy function
+def U(M, x, y):
+    neighbors = M[(x-1)%size, y] + M[(x+1)%size, y] + M[x, (y-1)%size] + M[x, (y+1)%size]
+    return M[x, y] * neighbors * 2
 
-  DU = M[x][y]*(M[left][y]+M[right][y]+M[x][top]+M[x][bottom])*(2)
-  return DU
-#Monte Carlo
-for x in range(0,iteration):
-  i = np.random.randint(0,size-1)
-  j = np.random.randint(0,size-1)
-  deltaU = U(i,j)
-  if deltaU <= 0 or np.random.rand() < np.exp(-deltaU / T):
-    M[i][j] *= -1
 
-#Plot
-plt.pcolormesh(M, edgecolors = 'k', cmap = 'gray')
+# Create 1x4 subplot
+fig, axes = plt.subplots(1, 4, figsize=(16, 4))
+
+# Plot 1: Initial state
+axes[0].pcolormesh(M_initial, edgecolors='k', cmap='gray')
+axes[0].set_title('Initial Random State')
+
+# Plots 2-4: Different temperatures
+for idx, T in enumerate(temperatures):
+    M = M_initial.copy()
+    
+    # Monte Carlo simulation
+    for x in range(iteration):
+        i = np.random.randint(0, size)
+        j = np.random.randint(0, size)
+        deltaU = U(M, i, j)
+        if deltaU <= 0 or np.random.rand() < np.exp(-deltaU / T):
+            M[i][j] *= -1
+    
+    axes[idx + 1].pcolormesh(M, edgecolors='k', cmap='gray')
+    axes[idx + 1].set_title(f'T = {T}')
+
+plt.tight_layout()
 plt.show()
